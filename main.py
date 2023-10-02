9906d726dc42e56597e4050e17837fd4a07c1f2c
OpiumVic <mutunga.victor@strathmore.edu>
2023-10-02 13:48:27 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import os
+import json
+import pandas as pd
+
+# Step 1: Specify the folder containing the .jsonl files
+dataset_folder = 'MASSIVE/data'  # Replace with the actual folder path
+
+# Initialize an empty list to store individual DataFrames
+dataframes = []
+
+# Step 2: Iterate through the .jsonl files in the folder
+for filename in os.listdir(dataset_folder):
+    if filename.endswith(".jsonl"):
+        file_path = os.path.join(dataset_folder, filename)
+
+        # Read each JSON line and convert it to a DataFrame
+        with open(file_path, 'r', encoding='utf-8') as jsonl_file:
+            data = [json.loads(line) for line in jsonl_file]
+            df = pd.DataFrame(data)
+            dataframes.append(df)
+
+# Step 3: Combine all DataFrames into one
+massive_dataset = pd.concat(dataframes, ignore_index=True)
+
+# Step 4: Create a single Excel file for all the data
+output_filename = 'massive_dataset.xlsx'
+massive_dataset.to_excel(output_filename, sheet_name='Sheet1', index=False)
+
+print("Excel file 'massive_dataset.xlsx' has been generated.")
