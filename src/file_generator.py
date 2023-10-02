ea16073c06fa823ec1a7157ea3f2c21f0ed6d115
OpiumVic <mutunga.victor@strathmore.edu>
2023-10-02 16:27:17 +0300

    Insert your Commit Message Here: Ensure you mention the changes you're making e.g, Introduced/Updated Authentication Module to do ...

--- src/file_generator.py
+++ src/file_generator.py
@@ -1,11 +1,35 @@
-# file_generator.py
+import os
+import json
+import pandas as pd
 
-class FileGenerator:
-    def __init__(self):
-        # Initialize the file generation settings here
-        pass
+# Step 1: Specify the folder containing the .jsonl files
+dataset_folder = 'data'  # Replace with the actual folder path
 
-    def generate_jsonl_files(self, dataset):
-        # Implement logic to generate JSONL files based on the dataset
-        # You can iterate through the dataset and create JSONL files
-        pass
+# Initialize an empty list to store individual DataFrames
+dataframes = []
+
+# Step 2: Iterate through the .jsonl files in the folder
+for filename in os.listdir(dataset_folder):
+    if filename.endswith(".jsonl"):
+        file_path = os.path.join(dataset_folder, filename)
+        processed = False  # Flag to track whether the file has been processed
+
+        # Read each JSON line and convert it to a DataFrame
+        with open(file_path, 'r', encoding='utf-8') as jsonl_file:
+            data = [json.loads(line) for line in jsonl_file]
+            df = pd.DataFrame(data)
+            dataframes.append(df)
+            processed = True  # Set the flag to True after processing
+
+        if processed:
+            # You can add further processing or flag-based actions here
+            print(f"File '{filename}' has been processed.")
+
+# Step 3: Combine all DataFrames into one
+massive_dataset = pd.concat(dataframes, ignore_index=True)
+
+# Step 4: Create a single Excel file for all the data
+output_filename = 'massive_dataset.xlsx'
+massive_dataset.to_excel(output_filename, sheet_name='Sheet1', index=False)
+
+print("Excel file 'massive_dataset.xlsx' has been generated.")
